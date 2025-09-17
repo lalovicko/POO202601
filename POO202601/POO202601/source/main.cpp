9638dca63fd5218b5e3862b797cc663dde784258
Jesus Eduardo <lalovicko@gmail.com>
2025-09-17 12:21:31 -0600

    Character

--- POO202601/POO202601/source/main.cpp
+++ POO202601/POO202601/source/main.cpp
@@ -1,6 +1,22 @@
 #include "Prerequisites.h"
+class
+	Character {
+public:
+	Character(int health) : m_health(health){}
+
+	int
+		getHealth()const {
+		return m_health;
+	}
+
+private:
+	int m_health;
+};
+
 int
 main() {
-	std::cout << "Hola mundo";
+	Character Churro(100);
+	Churro.getHealth();
+	std::cout << Churro.getHealth() << std::endl;
 	return 0;
 }
